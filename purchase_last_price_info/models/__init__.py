fdf754deebd66d2a6a77e435a5a03ce4a2aa77f1
oihane <oihanecruce@gmail.com>
2015-04-27 10:31:00 +0200

    [ADD] Moved <purchase_last_price_info> from odoomrp-wip
    [IMP] <purchase_last_price_info> Deleted dependency
    
    [IMP] <purchase_last_price> Changed to avoid initializing to False
    
    [IMP] <purchase_last_price_info> Minor changes
    
    [IMP] <purchase_last_price_info> added test
    
    [IMP] <purchase_last_price_info> OCA required _id added
    
    [FIX] <purchase_last_price_info> test fixed
    
    [IMP] Update <purchase_last_price_info> New version format and Try me button
    
    [IMP] <purchase_last_price_info> Test updated data when purchase.order is confirmed

--- purchase_last_price_info/models/__init__.py
+++ purchase_last_price_info/models/__init__.py
@@ -1,2 +1,11 @@
+<<<<<<< HEAD
 from . import product_product
 from . import product_template
+=======
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from . import product
+>>>>>>> [ADD] Moved <purchase_last_price_info> from odoomrp-wip
--- purchase_last_price_info/tests/__init__.py
+++ purchase_last_price_info/tests/__init__.py
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
+=======
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+>>>>>>> [ADD] Moved <purchase_last_price_info> from odoomrp-wip
 from . import test_purchase_last_price_info
